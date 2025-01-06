3984025d407b145c6d3cf7b4464e92f675034469
NIDHARSAN-V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-01-06 22:17:00 +0530

    Time: 1 ms (100%), Space: 47.1 MB (9.83%) - LeetHub

---
+++ 1254-deepest-leaves-sum/1254-deepest-leaves-sum.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int fin_level=0;
+    private int sum=0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        helper(root , 0);
+        return sum;
+    }
+    private void helper(TreeNode root , int level)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(level > fin_level && root.left==null && root.right==null)
+        {
+            fin_level = level;
+            sum = 0;
+        }
+        helper(root.left , level + 1);
+        helper(root.right , level + 1);
+        if(level == fin_level && root.left==null && root.right==null)
+        {
+            sum+=root.val;
+        }
+        return;
+    }
+}
