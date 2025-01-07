fdfe235e9065d87ddb6c3f124dd02903f438b653
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-01-07 21:54:03 +0530

    Time: 2 ms (31.64%), Space: 43 MB (12%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -6,7 +6,7 @@ class Solution {
             return 0;
         }
         
-        // Determine the sign
+   
         int sign = 1;
         int start = 0;
         if (s.charAt(0) == '-') {
@@ -21,20 +21,20 @@ class Solution {
         for (int i = start; i < s.length(); i++) {
             char ch = s.charAt(i);
             
-            // If the character is not a digit, return the result
+    
             if (!Character.isDigit(ch)) {
                 break;
             }
             
-            // Convert the character to a digit
+      
             int digit = ch - '0';
             
-            // Handle overflow by checking if the result is out of the 32-bit signed integer range
+   
             if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             
-            // Update result
+
             res = res * 10 + digit;
         }
         
