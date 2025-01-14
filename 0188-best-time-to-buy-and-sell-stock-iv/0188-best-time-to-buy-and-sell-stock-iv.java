d3bea8bf2bb732e5adc9e3996ded1b4c7e477be5
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-01-14 18:17:26 +0530

    Time: 7 ms (40.06%), Space: 45.4 MB (20.73%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,43 @@
+
+
+class Solution {
+    public int maxProfit(int k , int[] prices) {
+        int n = prices.length; 
+        int[][][] dp  = new int[n][2][k+1];
+
+        // Initialize the dp array with -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        // Call the helper function
+        return helper(prices, 0, 0, k, dp);
+
+
+
+    }
+    private int helper(int[] prices , int i , int buy , int maxc,int[][][] dp)
+    {
+        if(i==prices.length || maxc==0)
+        {
+            return 0;
+        }
+        if(dp[i][buy][maxc]!=-1)
+        {
+            return dp[i][buy][maxc];
+        }
+        dp[i][buy][maxc] = 0;
+        if(buy==0)
+        {
+            dp[i][buy][maxc] = Math.max(0 + helper(prices , i+1 , 0 , maxc ,dp) , -prices[i] + helper(prices , i+1 , 1 , maxc,dp));
+        }
+        else
+        {
+            dp[i][buy][maxc] = Math.max(0 + helper(prices , i+1 , 1 , maxc ,dp) , prices[i] + helper(prices , i+1 , 0 , maxc -1 , dp));
+        }
+
+        return dp[i][buy][maxc];
+    }
+}
