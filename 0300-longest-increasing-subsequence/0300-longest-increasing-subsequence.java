4323bda72136625efd6aa1edda0a8bcb1da1d89d
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-01-15 08:46:14 +0530

    Time: 445 ms (5.05%), Space: 80 MB (5.26%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length + 1];
+        for(int[] d : dp)
+        {
+            Arrays.fill(d ,-1);
+        }
+        return helper(nums , 0 , -1 ,dp);
+
+    }
+    private int helper(int[] nums , int i , int prev , int[][] dp)
+    {
+        if(i==nums.length)
+        {
+            return 0;
+        }
+
+        if(dp[i][prev + 1]!=-1)
+        { 
+           return dp[i][prev+1];
+        }
+
+        dp[i][prev+1] = 0 +  helper(nums , i + 1 , prev , dp);
+        if(prev == -1 || nums[i] > nums[prev])
+        {
+        dp[i][prev+1]= Math.max(dp[i][prev+1] , 1 + helper(nums , i + 1 , i , dp));
+        }
+        return dp[i][prev+1];
+    }
+}
+//prev + 1 because of the -1 value of prev
