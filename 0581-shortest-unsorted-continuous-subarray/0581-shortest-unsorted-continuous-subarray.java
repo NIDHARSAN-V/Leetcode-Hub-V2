a7a1a40561b0201e4f09f36e4456405bb0ae3608
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-01-20 23:40:27 +0530

    Time: 4 ms (44.79%), Space: 46 MB (5.12%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -1,12 +1,27 @@
 class Solution {
-    public int findUnsortedSubarray(int[] A) {
-    int n = A.length, beg = -1, end = -2, min = A[n-1], max = A[0];
-    for (int i=1;i<n;i++) {
-      max = Math.max(max, A[i]);
-      min = Math.min(min, A[n-1-i]);
-      if (A[i] < max) end = i;
-      if (A[n-1-i] > min) beg = n-1-i; 
+    public int findUnsortedSubarray(int[] nums) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        // startIdx, endIdx, value
+        int start = -1, end = -1, val = Integer.MIN_VALUE;
+        int pi = -1; // poll Index
+        for(int i = 0; i < nums.length; i++){
+            while(!stack.isEmpty() && nums[stack.peekLast()] > nums[i]){
+                pi = stack.pollLast();
+                // set the start to leftMost index which is not in order
+                if(start == -1 || pi < start) start = pi;
+                // find the maximum value of elements in stack (which are being polled)
+                // reasoning : maximum element's index will be the rightmost after sorting 
+                // (or) inserting in the right place
+                val = Math.max(nums[pi], val);
+            }
+            // offer the current Index (this index is in correct position)
+            stack.offer(i);
+            // if maximum value can be inserted after current element(nums[i]) then that position will be current index + 1. 
+            if(val > nums[i]) end = i + 1;
+            // if it can't be inserted, then the element was already inserted previously. Now we can reset the value, hoping to catch another value if there are unordered elements in the future.
+            else val = Integer.MIN_VALUE;
+        }
+        // finally return the length from the range observed
+        return end - start;
     }
-    return end - beg + 1;
-}
 }
