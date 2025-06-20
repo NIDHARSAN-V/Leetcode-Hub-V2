9607ca6a16f7f120c0512f85f7efded621fe6257
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-06-20 21:49:28 +0530

    Time: 14 ms (64.04%), Space: 48.1 MB (61.1%) - LeetHub

--- 0554-brick-wall/0554-brick-wall.java
+++ 0554-brick-wall/0554-brick-wall.java
@@ -1,31 +1,17 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-
-        Map<Integer , Integer> res = new HashMap<>();
-        int count =0;
+        Map<Integer, Integer> map = new HashMap();
         
-        prefixhelper(wall);
-
-        for(List<Integer> li: wall )
-        {
-            for(int i = 0 ; i < li.size()-1 ; i++)
-            {
-                res.put( li.get(i) , res.getOrDefault(li.get(i) , 0)  +1);
-                count = Math.max(res.get(li.get(i)) , count);
+        int count = 0;
+        for (List<Integer> row : wall) {
+            int sum = 0;
+            for (int i = 0; i < row.size() - 1; i++) {
+                sum += row.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                count = Math.max(count, map.get(sum));
             }
         }
-        return Math.abs(count - wall.size());
-
-
         
-    }
-
-
-    private void prefixhelper(List<List<Integer>> lists) {
-        for (List<Integer> sublist : lists) {
-            for (int i = 1; i < sublist.size(); i++) {
-                sublist.set(i, sublist.get(i) + sublist.get(i - 1));
-            }
-        }
+        return wall.size() - count;
     }
 }
