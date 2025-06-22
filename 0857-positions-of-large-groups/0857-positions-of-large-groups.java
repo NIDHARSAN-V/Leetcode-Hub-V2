2361d5fd362c6e6c14fcc99d2a5e78254ec7c399
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-06-22 22:23:15 +0530

    Time: 2 ms (32.56%), Space: 44.9 MB (22.82%) - LeetHub

--- 0857-positions-of-large-groups/0857-positions-of-large-groups.java
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.java
@@ -1,18 +1,43 @@
 class Solution {
     public List<List<Integer>> largeGroupPositions(String s) {
-        List<List<Integer>> res = new ArrayList<>();
-        int start = 0;
+        // int off = 3;
 
-        for (int end = 1; end <= s.length(); end++) {
-     
-            if (end == s.length() || s.charAt(end) != s.charAt(start)) {
-                if (end - start >= 3) {
-                    res.add(new ArrayList<>(Arrays.asList(start, end - 1)));
-                }
-                start = end; 
+        List<List<Integer>> res = new ArrayList<>();
+
+        int start =0;
+        int end = 0;
+        int count =1;
+
+        while(end < s.length() && start < s.length())
+        {
+            if(start == end)
+            {
+                count=1;
+                end++;
+                
             }
+
+            if(  end < s.length() && s.charAt(end) == s.charAt(start))
+            {
+               end++;
+               count++; 
+            }
+            else
+            {
+                if(count >= 3)
+                {
+                    res.add(new ArrayList<>(Arrays.asList(start, end-1)));
+                }
+                start = end;
+
+            }
+
+            
+        }//edge
+         if (count >= 3) {
+            res.add(new ArrayList<>(Arrays.asList(start, end - 1)));
         }
 
-        return res;
+    return res;
     }
 }
