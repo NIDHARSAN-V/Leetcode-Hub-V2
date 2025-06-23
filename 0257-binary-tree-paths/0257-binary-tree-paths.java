ea4f3b4b90ad47a2a2e1edbc8b20d4224b225314
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-06-23 22:11:54 +0530

    Time: 8 ms (37.82%), Space: 42.8 MB (36.08%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -4,35 +4,24 @@ class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
 
         List<String> res  = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
-        helper(root , sb , res);
+     
+    helper(root ,"" , res);
         return res;
 
     }
 
-    private void helper(TreeNode root , StringBuilder sb , List<String> res )
-    {
-        
-         if(root ==null)
-         {
-            return;
-         }
-        int len = sb.length();
-        sb.append(root.val);
+   private void helper(TreeNode root, String path, List<String> res) {
+    if (root == null) return;
 
+    path += root.val;
 
-        if (root.left == null && root.right == null) {
-            res.add(sb.toString());
-        }
-        else
-        {
-                sb.append("->");
-                helper(root.left , sb , res);
-                helper(root.right , sb , res);
-
-        }
-        sb.setLength(len);
-
-        return;  
+    if (root.left == null && root.right == null) {
+        res.add(path);
+    } else {
+        path += "->";
+        helper(root.left, path, res);
+        helper(root.right, path, res);
     }
+}
+
 }
