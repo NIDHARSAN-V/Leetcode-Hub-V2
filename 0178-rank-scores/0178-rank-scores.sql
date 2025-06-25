4fc6d3e887edb3b46f45942d84ccf04711f9244d
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-06-25 18:41:01 +0530

    Time: 274 ms (96.61%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,9 +1,1 @@
-SELECT 
-  p.Score,  
-  (
-    SELECT COUNT(DISTINCT s.Score) 
-    FROM Scores s 
-    WHERE p.Score <= s.Score
-  ) AS `rank`
-FROM Scores p
-ORDER BY `rank`;
+select score , DENSE_RANK() over (order by score desc) as "Rank" from Scores;
