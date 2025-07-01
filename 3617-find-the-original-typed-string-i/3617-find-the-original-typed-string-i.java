4bb6c8a9179656e093ea9735b42ce1577c081430
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-07-01 22:53:27 +0530

    Time: 0 ms (100%), Space: 42 MB (88.84%) - LeetHub

---
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int count = 1;
+        char prev = word.charAt(0);
+        for(int i = 1 ; i < word.length() ;i++)
+        {
+            if(word.charAt(i) == prev)
+            {
+                count++;
+            }
+            else
+            {
+                prev = word.charAt(i);
+            }
+        }
+
+        return count ;
+    }
+}
