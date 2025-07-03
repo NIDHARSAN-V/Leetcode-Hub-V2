77b9b6cc17f3757cd1d3ca5045850fa770b15bb8
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-07-03 19:45:25 +0530

    Time: 0 ms (100%), Space: 41.4 MB (91.15%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -1,19 +1,19 @@
 class Solution {
     public char kthCharacter(int k) {
-        String s = "a";
+         int index = k - 1;
+        int increments = 0;
 
-        while (s.length() < k) {
-            StringBuilder x = new StringBuilder();
-            for (char c : s.toCharArray()) {
-                if (c == 'z') {
-                    x.append('a');
-                } else {
-                    x.append((char)(c + 1));
-                }
+        // Count how many times we "shifted" this character through the generations
+        while (index > 0) {
+            long p = 1;
+            while (p * 2 <= index) {
+                p *= 2;
             }
-            s += x.toString();
+            increments++;
+            index -= p;
         }
 
-        return s.charAt(k - 1);
+        // Compute the final character with wrap-around from 'z' to 'a'
+        return (char) ('a' + (increments % 26));
     }
 }
