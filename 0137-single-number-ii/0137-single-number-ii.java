d9b5155893c6303756bbe70396df9ef0876f6960
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-08-27 22:35:24 +0530

    Time: 1 ms (80.55%), Space: 45.8 MB (20.38%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int ones = 0;
+        int twos = 0;
+
+
+        for(int n : nums)
+        {
+            ones = (ones ^ n) & ~twos;
+            twos = (twos ^ n) & ~ones;
+        }
+
+        return ones;
+    }
+}
