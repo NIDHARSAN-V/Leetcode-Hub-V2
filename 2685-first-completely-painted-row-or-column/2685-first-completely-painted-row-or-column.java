b765c53863543bfb9bd9d46438e0b494aae038ae
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-08-30 15:27:20 +0530

    Time: 22 ms (84.02%), Space: 69.4 MB (31.96%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -1,28 +1,26 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, int[]> map = new HashMap<>();
         int m = mat.length;
         int n = mat[0].length;
 
-        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                map.put(mat[i][j], new ArrayList<>(Arrays.asList(i, j)));
+                map.put(mat[i][j], new int[]{i, j});
             }
         }
 
         int[] rc = new int[m];
-        int[] cc = new int[n]; 
+        int[] cc = new int[n];
 
         for (int i = 0; i < arr.length; i++) {
-            List<Integer> pos = map.get(arr[i]);
-            int r = pos.get(0);
-            int c = pos.get(1);
+            int[] pos = map.get(arr[i]);
+            int r = pos[0];
+            int c = pos[1];
 
             rc[r]++;
             cc[c]++;
-
 
             if (rc[r] == n || cc[c] == m) {
                 return i;
