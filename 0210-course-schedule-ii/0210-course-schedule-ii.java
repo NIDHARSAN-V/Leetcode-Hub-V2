1d6c0098484a409b81558a669076bd98ad7ea194
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-09-23 15:12:57 +0530

    Time: 6 ms (43.17%), Space: 45.9 MB (65.63%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> premap = new HashMap<>();
+
+      
+        for (int[] x : prerequisites) {
+            premap.putIfAbsent(x[0], new ArrayList<>());
+            premap.get(x[0]).add(x[1]);
+        }
+
+        int[] vis = new int[numCourses]; 
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, premap, vis)) {
+                return new int[0];
+            }
+        }
+
+       
+        // Collections.reverse(res);
+
+
+        int[] result = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            result[i] = res.get(i);
+        }
+
+        return result;
+    }
+
+    private boolean dfs(int course, Map<Integer, List<Integer>> map, int[] vis) {
+        if (vis[course] == 1) return false; 
+        if (vis[course] == 2) return true;  
+
+        vis[course] = 1;
+        if (map.containsKey(course)) {
+            for (int pre : map.get(course)) {
+                if (!dfs(pre, map, vis)) {
+                    return false;
+                }
+            }
+        }
+
+        res.add(course); 
+        vis[course] = 2;  
+        return true;
+    }
+}
