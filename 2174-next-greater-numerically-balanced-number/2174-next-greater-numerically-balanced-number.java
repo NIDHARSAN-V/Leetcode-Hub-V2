a98c735466acea9bb45fcafff71d4772c8df9f9d
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-10-24 18:41:32 +0530

    Time: 133 ms (28.46%), Space: 44.7 MB (23.08%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
@@ -2,27 +2,23 @@ class Solution {
     int res = Integer.MIN_VALUE;
     public int nextBeautifulNumber(int n) 
     {
-         int x = n + 1;
+        int x = n + 1;
         while (true) {
-        if (isBalanced(x)) return x;
-        x++;
-    }
+            if (isBalanced(x)) return x;
+            x++;
+        }
        
     }
 
    boolean isBalanced(int num) {
-    int[] freq = new int[10];
-    char[] arr = String.valueOf(num).toCharArray();
-    for (char c : arr) freq[c - '0']++;
+        int[] freq = new int[10];
+        char[] arr = String.valueOf(num).toCharArray();
+        for (char c : arr) freq[c - '0']++;
 
-    for (int i = 0; i < 10; i++) {
-        if (freq[i] > 0 && freq[i] != i) return false;
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0 && freq[i] != i) return false;
+        }
+        return true;
     }
-    return true;
-}
 
-
-
-
-   
 }
