5a25c2b35e2f01cc0d3b882018adf78a2ed61ef2
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-10-24 19:15:30 +0530

    Time: 134 ms (26.92%), Space: 44.2 MB (75.38%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.java
@@ -1,37 +1,28 @@
 class Solution {
-    int res = Integer.MAX_VALUE; 
-
-    public int nextBeautifulNumber(int n) {
-        int maxlen = String.valueOf(n).length() + 1;
-        helper(n, 0, maxlen, new int[10]);
-        return res;
+    int res = Integer.MIN_VALUE;
+    public int nextBeautifulNumber(int n) 
+    {
+         int x = n + 1;
+        while (true) {
+        if (isBalanced(x)) return x;
+        x++;
+    }
+       
     }
 
-    public void helper(int n, int num, int maxlen, int[] freq) {
-        
-        if (num > n && balanced(freq)) {
-            res = Math.min(res, num);
-        }
+   boolean isBalanced(int num) {
+    int[] freq = new int[10];
+    char[] arr = String.valueOf(num).toCharArray();
+    for (char c : arr) freq[c - '0']++;
 
-        
-        if (String.valueOf(num).length() > maxlen || num > 1223333) {
-            return;
-        }
-
-        
-        for (int i = 1; i <= 7; i++) {
-            if (freq[i] < i) { 
-                freq[i]++;
-                helper(n, num * 10 + i, maxlen, freq);
-                freq[i]--;
-            }
-        }
+    for (int i = 0; i < 10; i++) {
+        if (freq[i] > 0 && freq[i] != i) return false;
     }
+    return true;
+}
 
-    private boolean balanced(int[] count) {
-        for (int d = 1; d <= 7; d++) {
-            if (count[d] != 0 && count[d] != d) return false;
-        }
-        return true;
-    }
+
+
+
+   
 }
