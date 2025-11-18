74da626daa6d93318a5ccc19e68fb97bffd53960
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2025-11-18 22:05:28 +0530

    Time: 0 ms (100%), Space: 44.4 MB (10.22%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.java
@@ -4,9 +4,7 @@ class Solution {
         while(i < bits.length - 1)
         {
             
-            i += bits[i] + 1;
-            
-           
+            i += bits[i] + 1;  
         }
         return i==bits.length - 1;
         
