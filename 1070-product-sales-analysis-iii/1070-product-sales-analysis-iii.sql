41cc334c95e6416e10450d6a269a91377aa73c9b
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2026-04-06 01:43:09 +0530

    Time: 822 ms (18.44%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+WITH sample AS (
+    SELECT *,
+           MIN(year) OVER (PARTITION BY product_id) AS first_year
+    FROM Sales
+)
+SELECT 
+    product_id,
+    first_year,
+    quantity,
+    price
+FROM sample
+WHERE year = first_year;
