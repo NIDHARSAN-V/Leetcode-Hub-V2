5fc4aed0354245fc4577a5d63c71c981f2951c37
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2026-04-09 01:11:53 +0530

    Time: 568 ms (30.73%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,35 +1,34 @@
-WITH sample AS (
-    SELECT 
+with sample as (
+    select 
         t.id,
         t.client_id,
         t.driver_id,
         t.city_id,
         t.status,
         t.request_at,
-        c.banned AS client_banned,
-        d.banned AS driver_banned
-    FROM Trips t
-    LEFT JOIN Users c 
-        ON t.client_id = c.users_id
-    LEFT JOIN Users d 
-        ON t.driver_id = d.users_id
+        c.banned as client_banned,
+        d.banned as driver_banned
+    from Trips t
+    left join Users c 
+        on t.client_id = c.users_id
+    left join Users d 
+        on t.driver_id = d.users_id
 )
 
-SELECT 
-    request_at AS Day,
-    ROUND(
-        SUM(
-            CASE 
-                WHEN status != 'completed' THEN 1 
-                ELSE 0 
-            END
-        )  / COUNT(*), 
+select 
+    request_at as Day,
+    round(
+        sum(
+            case 
+                when status != 'completed' then 1 
+                else 0 
+            end
+        ) * 1.0 / count(*), 
         2
-    ) AS "Cancellation Rate"
-
-FROM sample
-WHERE 
+    ) as "Cancellation Rate"
+from sample
+where 
     client_banned = 'No'
-    AND driver_banned = 'No'
-    AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
-GROUP BY request_at;
+    and driver_banned = 'No'
+    and request_at between '2013-10-01' and '2013-10-03'
+group by request_at;
