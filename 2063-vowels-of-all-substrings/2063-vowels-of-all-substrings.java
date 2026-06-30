31bdce897b7a5457e05db507d97e3a45b582ebdb
Nidharsan V <125539361+NIDHARSAN-V@users.noreply.github.com>
2026-07-01 00:38:31 +0530

    Time: 8 ms (91.9%), Space: 46.7 MB (56.68%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public long countVowels(String word) {
+
+        long count = 0;
+        int n = word.length();
+
+        for (int i = 0; i < n; i++) {
+
+            char ch = word.charAt(i);
+
+           
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+
+              
+                count += (long) (i + 1) * (n - i);
+            }
+        }
+
+        return count;
+    }
+}
